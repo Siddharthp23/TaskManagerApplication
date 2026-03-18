9483a66cf8f4e0d21296aeed8f4d18c6bdf2e28e
Siddharthp23 <pagaresiddharth239@gmail.com>
2026-03-18 12:55:10 +0530

    fix:Modify frontend code for deployment

--- Frontend/task_manager_application/src/api.js
+++ Frontend/task_manager_application/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://taskmanagerapplication-yobl.onrender.com";
 
 export const getTasks = async () => {
   const res = await fetch(`${BASE_URL}/tasks`);
